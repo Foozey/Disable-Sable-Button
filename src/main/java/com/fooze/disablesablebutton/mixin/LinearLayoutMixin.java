1cd20fbc6aabdb78cd9266f742ede4f4c2cedb08
Isaac <isaacallencore+productivity@protonmail.com>
2026-08-24 17:49:42 +1000

    Refactor code and add comments

--- src/main/java/com/fooze/disablesablebutton/mixin/LinearLayoutMixin.java
+++ src/main/java/com/fooze/disablesablebutton/mixin/LinearLayoutMixin.java
@@ -1,10 +1,10 @@
 package com.fooze.disablesablebutton.mixin;
 
-import com.fooze.disablesablebutton.SableButtonHelper;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.layouts.LayoutElement;
 import net.minecraft.client.gui.layouts.LayoutSettings;
 import net.minecraft.client.gui.layouts.LinearLayout;
+import net.minecraft.network.chat.contents.TranslatableContents;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,10 +12,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LinearLayout.class)
 public abstract class LinearLayoutMixin {
+    // Prevents the Sable menu button from being added to the layout
     @Inject(method = "addChild(Lnet/minecraft/client/gui/layouts/LayoutElement;Lnet/minecraft/client/gui/layouts/LayoutSettings;)Lnet/minecraft/client/gui/layouts/LayoutElement;", at = @At("HEAD"), cancellable = true)
     private <T extends LayoutElement> void disablesablebutton$skipSableButton(
-            T child, LayoutSettings settings, CallbackInfoReturnable<T> callback) {
-        if (child instanceof Button button && SableButtonHelper.isSableButton(button)) {
+            T child, LayoutSettings settings, CallbackInfoReturnable<T> callback
+    ) {
+        if (child instanceof Button button
+                && button.getMessage().getContents() instanceof TranslatableContents contents
+                && contents.getKey().equals("options.sable_menu")) {
             callback.setReturnValue(child);
         }
     }
